e3298ccbf95345fec1e1795b94da26d7bf96e6fe
cheng <1814638551@qq.com>
2019-02-15 09:23:54 +0800

    cheng

--- src/components/header/searchBar.vue
+++ src/components/header/searchBar.vue
@@ -8,40 +8,22 @@
             </el-col>
             <el-col :span='15' class="center"> 
                 <div class="wrapper">
-                    <el-input v-model="searchWord" placeholder="搜索商家或地点"></el-input>
+                    <el-input v-model="searchWord" placeholder="搜索商家或地点" @focus="focus" @blur="blur"></el-input>
                     <el-button type="primary" icon="el-icon-search"></el-button>
                     <dl class="hot-place" v-if="isHotPlace">
                         <dt>热门搜索</dt>
-                        <dd>
-                            <router-link to="/">京东第一温泉</router-link>
-                        </dd>
-                        <dd>
-                            <router-link to="/">度假村7天连锁酒店</router-link>
-                        </dd>
-                        <dd>
-                           <router-link to="/">格林豪泰快捷酒店兰庭酒店</router-link> 
-                        </dd>
-                        <dd>
-                            <router-link to="/">99旅馆连锁尚华酒店</router-link>
+                        <dd v-for="(item, index) in hotPlaceList" :key="index">
+                            <router-link :to="{name:'goods', params: {name: item}}">{{item}}</router-link>
                         </dd>
                     </dl>
                     <dl class="search-list" v-if="isSearchList">
-                        <dd>
-                            <router-link to="/">火锅</router-link>
-                        </dd>
-                        <dd>
-                            <router-link to="/">火锅底料</router-link>
-                        </dd>
-                        <dd>
-                            <router-link to="/">火锅自助</router-link>
+                         <dd v-for="(item, index) in searchaList" :key="index">
+                            <router-link :to="{name:'goods', params: {name: item}}">{{item}}</router-link>
                         </dd>
                     </dl>
                 </div>
                 <p class="suggest"> 
-                    <router-link to="/">京东第一温泉</router-link>
-                    <router-link to="/">度假村7天连锁酒店</router-link>
-                    <router-link to="/">格林豪泰快捷酒店兰庭酒店</router-link>
-                    <router-link to="/">99旅馆连锁尚华酒店</router-link>
+                    <a href="#" v-for="(item, index) in suggestList" :key="index">{{item}}</a>
                 </p>
             </el-col>
         </el-row>
@@ -52,8 +34,31 @@ export default {
     data(){
         return {
             searchWord: '',
-            isHoisHotPlace: false,
-            isSearchList: false,
+            // isHotPlace: true,
+            // isSearchList: true,
+            hotPlaceList: ["京东第一温泉", "度假村7天连锁酒店", "格林豪泰快捷酒店兰庭酒店"], //热门搜索列表
+            searchaList: ["火锅", "火锅底料", "火锅自助"],
+            suggestList:["京东第一温泉", "度假村7天连锁酒店", "格林豪泰快捷酒店兰庭酒店", "99旅馆连锁尚华酒店"],
+            isFous: false,
+        }
+    },
+    computed: {
+        isHotPlace() {
+            return this.isFous && !this.searchWord;
+        },
+        isSearchList() {
+            return this.isFous && this.searchWord;
+        },
+    },
+    methods: {
+        focus() {
+            this.isFous = true
+        },
+        blur() {
+            let self = this
+            setTimeout(() => {
+                self.isFous = false
+            }, 500);
         }
     }
 }
@@ -86,7 +91,7 @@ export default {
             }
         }
         .center .wrapper {
-            
+            position: relative;
             display: flex;
             padding-top: 28px;
             .el-input {
@@ -113,6 +118,66 @@ export default {
                 border-bottom-left-radius: 0;
                 background-color: #13D1BE;
                 border: 1px solid transparent;
+            }
+            .hot-place{
+                position: absolute;
+                left: 0;
+                top: 68px;
+                z-index: 999;
+                width: 462px;
+                padding:10px 5px;
+                box-sizing: border-box;
+                font-size: 14px;
+                line-height: 2;
+                border: 1px solid #ddd;
+                border-top: none;
+                border-bottom-right-radius: 4px;
+                border-bottom-left-radius:4px;
+                background-color: #fff;
+                box-shadow: 0 3px 5px 0 rgba(0,0,0,0.1);
+                dt {
+                    padding: 0 5px;
+                    font-weight: bold;
+                    color:#9c9c9c;
+                }
+                dd {
+                    float: left;
+                    padding-left: 5px;
+                    a{
+                        color: #333;
+                    }
+                    a:hover {
+                        color: #13D1BE;
+                    }
+                }
+            }
+            .search-list{
+                position: absolute;
+                left: 0;
+                top: 68px;
+                z-index: 999;
+                width: 462px;
+                box-sizing: border-box;
+                font-size: 14px;
+                line-height: 2;
+                border: 1px solid #ddd;
+                border-top: none;
+                border-bottom-right-radius: 4px;
+                border-bottom-left-radius:4px;
+                background-color: #fff;
+                box-shadow: 0 3px 5px 0 rgba(0,0,0,0.1);
+                a {
+                    padding: 0 5px;
+                    box-sizing: border-box;
+                    display: inline-block;
+                    width: 100%;
+                    cursor: pointer;
+                    color: #999;
+                }
+                a:hover{
+                    background-color: #f8f8f8;
+                    color: #13D1BE;
+                }
             }
         }
         .suggest {
